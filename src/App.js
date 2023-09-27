639d4d55a6719b858eb93e4f3c611b067a76f91c
FlorFuentes <flopy_200412@hotmail.com>
2023-09-27 20:02:35 -0300

    carga CV

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Usuario from './componentes/Usuario';
 function App() {
   return (
     <div className="App">
-
-      <Usuario nombre="Florencia" cargo="Desarrolladora Web" conoceme="Cv de Florencia" imagen={require('./componentes/img/stych.png')} />
-      <Usuario nombre="Carlos" cargo="Tecnico en Sofware" conoceme="Cv de Carlos" imagen={require('./componentes/img/stych.png')} />
-      <Usuario nombre="Martina" cargo="Desarrollador Back End" conoceme="Cv de Martina" imagen={require('./componentes/img/stych.png')} />
+      
+      
+        
+        <Usuario nombre="Florencia" cargo="Desarrolladora Web" descargarCV="Cv de Florencia" imagen={require('./componentes/img/stych.png')}/>
+        <Usuario nombre="Carlos" cargo="Tecnico en Sofware" descargarCV="Cv de Carlos" imagen={require('./componentes/img/stych.png')}/>
+        <Usuario nombre="Martina" cargo="Desarrollador Back End" descargarCV="Cv de Martina" imagen={require('./componentes/img/stych.png')} /> 
+      
     </div>
   );
 }
--- src/componentes/Usuario.jsx
+++ src/componentes/Usuario.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './usuario.css'; 
+import html2canvas from 'html2canvas';
 
 function Usuario(props) {
+
+    const descargarCV = () => {
+        html2canvas(document.querySelector("#exportar")).then(function (canvas) {
+            let img = canvas.toDataURL("image/png"); 
+            let link = document.createElement("a");
+            link.download = "cv.png"; 
+            link.href = img;
+            link.click();
+        });
+    }
+
     return (
         <div>
-            <div className="card" style={{width: '18rem'}}>
+            <div id='exportar' className="card" style={{width: '18rem'}}>
                 <img src={props.imagen} className="card-img-top" alt="Paisaje"/>
                 <div className="card-body">
                     <h1>{props.nombre}</h1>
                     <p className="card-text">{props.cargo}</p>
-                    <a href="" className='btn btn-color'>{props.conoceme}</a>
+                    <button onClick={descargarCV} className='btn-color'>Descargar CV</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default Usuario
+export default Usuario;
